52e530d5ce6b8d0e3bdf87e2f8c06d1b26c001b3
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-12-04 22:40:58 +0200

    add hw2 solution

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let randomNumber = Math.floor(Math.random() * 100);
+
+if (randomNumber % 2 === 0) {
+  console.log(`${randomNumber}: Это четное число`);
+} else {
+  console.log(`${randomNumber}: Это нечетное число`);
+}
