54176502f68c64f3b9e1d7966b6f97df1da28ec6
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-12-13 14:43:56 +0200

    fixes 2_5 after samuray helps

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -6,5 +6,5 @@ let passport = {
 let passport1 = {...passport};
 passport1.name = 'Ivan';
 
-console.log(passport);
-console.log(passport1);
+console.log(passport.name);
+console.log(passport1.name);
